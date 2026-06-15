2047ac23c650d9a6264ddd6c6238f70a0ed32772
Ronald Chonillo <ronald.chonillo@gmail.com>
2026-06-14 19:26:44 -0500

    [add] package manager solo falta la descarga

---
+++ WebMobileManager/WebMobileManager.Web/Components/Pages/Packages/PackageFileDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebMobileManager.Web.Components.Pages.Packages
+{
+    public class PackageFileDto
+    {
+        public string file_name { get; set; }
+        public string file_type { get; set; }
+        public string status { get; set; }
+        public bool success_upload { get; set; }
+    }
+}
---
+++ WebMobileManager/WebMobileManager.Web/Components/Pages/Packages/PackageViewDto.cs
@@ -0,0 +1,17 @@
+﻿using WebMobileManager.Web.Services.Sqlite;
+
+namespace WebMobileManager.Web.Components.Pages.Packages
+{
+    public class PackageViewDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string database_name { get; set; }
+        public string file_name { get; set; }
+        public bool success_upload { get; set; }
+        public string processing_state { get; set; }
+        public DateTime created_at { get; set; }
+
+        public List<PackageFileDto> Files { get; set; } = new();
+    }
+}
