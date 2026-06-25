66751d29f1c1bb44c1c2c97fd0210e805abb9cb2
Ronald Chonillo <ronald.chonillo@gmail.com>
2026-06-25 17:26:04 -0500

    [fix] aligntext

--- MauiApp100/Controls/SaleOrderLineSkiaView.xaml.cs
+++ MauiApp100/Controls/SaleOrderLineSkiaView.xaml.cs
@@ -191,11 +191,10 @@ public partial class SaleOrderLineSkiaView : ContentView
         var fontAwesome = new SKFont { Typeface = _fontAwesomeTypeface, Size = 24 * ScaleText };
         var fontBold = new SKFont { Size = 22 * ScaleText, Embolden = true };
         var fontRight = new SKFont { Size = 22 * ScaleText, Embolden = true };
-        var textAlign = SKTextAlign.Right;
-        
+                
         // COLUMN 0
         paint.Color = SKColors.Green;
-        canvas.DrawText(Item.sequence.ToString(), colX[0] + 2, row1Y, textAlign, fontNormal, paint);
+        canvas.DrawText(Item.sequence.ToString(), colX[0] + 2, row1Y, SKTextAlign.Right, fontNormal, paint);
 
         // COLUMN 1
         canvas.DrawText(Item.product_code ?? "", colX[1], row1Y, fontBold, paint);
