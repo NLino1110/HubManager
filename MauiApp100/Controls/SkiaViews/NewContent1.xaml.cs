e85b85ce2e659dc1856d9f2228dccea7a45ef2a0
Ronald Chonillo <ronald.chonillo@gmail.com>
2026-06-26 15:16:42 -0500

    [add] Generator

---
+++ MauiApp100/Controls/SkiaViews/NewContent1.xaml.cs
@@ -0,0 +1,10 @@
+namespace MauiApp100.Controls.Views;
+
+public partial class NewContent1 : ContentView
+{
+	public string Title { get; set; }
+	public NewContent1()
+	{
+		InitializeComponent();        
+    }
+}
